3e5bebd908836250b7bc859665feaca38eacb550
alertocontador <anunayb88@gmail.com>
2025-08-25 09:24:25 +0000

    changes

--- service-a/index.js
+++ service-a/index.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
-const PORT = process.env.PORT || 7000;
+const PORT = 7000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'application/json' });
